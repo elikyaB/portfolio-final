aaf288f992341d4c71ddba7495feb6dd4cd0c149
elikya <elikyabokanga@gmail.com>
2022-01-17 17:29:10 -0500

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+<script>
+	import {onMount} from "svelte"
+	import Landing from "./pages/Landing.svelte";
+	import About from "./pages/About.svelte";
+	import Portfolio from "./pages/Portfolio.svelte";
+	import Contact from "./pages/Contact.svelte";
+	import Footer from "./components/Footer.svelte";
+</script>
+
+<main>
+	<Landing />
+	<About />
+	<Portfolio />
+	<Contact />
+	<Footer />
+</main>
+
+<style>
+
+</style>
