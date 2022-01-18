8eb2b2fce0946ebd3dd309c079d3ef55955be1d0
elikya <elikyabokanga@gmail.com>
2022-01-17 23:05:28 -0500

    test applied to site

--- src/pages/Landing.svelte
+++ src/pages/Landing.svelte
@@ -1,7 +1,21 @@
 <script>
+    import { onMount } from 'svelte'
     import {fade, fly} from 'svelte/transition'
     import {elasticOut} from 'svelte/easing'
-    import Test from '../components/Test.svelte';
+    import Test from '../components/Test.svelte'
+
+    const titles = ["coder", "programmer", "developer", "engineer"]
+    let title = "coder"
+
+    function titleSwitch (t) {
+        console.log(title)
+        const current = titles.findIndex((w) => {return w === t})
+        const next = current < titles.length-1 ? current+1 : 0
+        title = titles[next]
+        setTimeout(() => {titleSwitch(title)}, 3000)
+    }
+
+    onMount(() => titleSwitch(title))
     
 
     function wordlock (node, {duration}) {
@@ -15,17 +29,11 @@
             }
         }
     }
-
-    function titleSwitch (title) {
-        const titles = ["coder", "programmer", "developer", "engineer"]
-        const current = titles.findIndex(title)
-    }
-
-    let title = setTimeout(titleSwitch(title), 5000)
 </script>
 
 <div>
     <h1>Hi!<br/>I'm Eli,</h1>
+    <h1>{title}</h1>
     <Test />
 </div>
 
