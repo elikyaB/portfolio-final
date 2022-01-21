a43f4d7f974423e8aeb9ce59e8701028ed22b80f
elikya <elikyabokanga@gmail.com>
2022-01-21 14:30:08 -0500

    done tinkering with navbar for now

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,65 +1,38 @@
 <script>
     let active = false
     function activate(){active = !active}
-    function test(){console.log('works')}
-    let hide = false
-    function conceal(){hide = !hide}
+    function test(){console.log('test')}
 </script>
 
 <nav class="navbar" aria-label="main navigation">
     <div class="navbar-brand">
         <a href="/" class="navbar-item">Eli B3 | Web Dev</a>
         <div 
-            class="{`navbar-burger has-dropdown is-arrowless${active?' is-active':''}`}"
-            on:click={test,activate}
+            class="{`navbar-burger has-dropdown is-arrowless is-right${active?' is-active':''}`}"
+            on:click|stopPropagation={activate}
+            on:pointerenter={activate}
+            on:pointerleave={activate}
         >
             <span aria-hidden="true"></span>
             <span aria-hidden="true"></span>
             <span aria-hidden="true"></span>
-            <!-- <div class="{`navbar-dropdown is-right${active?' is-active':''}`}">
-                <div class="navbar-item">About</div>
-                <div class="navbar-item">Portfolio</div>
-                <div class="navbar-item">Contact</div>
-                <hr class="navbar-divider"/>
-                <div class="navbar-item">Resume</div>
-                <div class="navbar-item">Github</div>
-                <div class="navbar-item">Blog</div>
-            </div> -->
         </div>
     </div>
-    <div id="pages" class="{`navbar-menu justify-content${active?' is-active':''}`}">
+    <div class="{`navbar-menu ${active?' is-active':''}`}">
         <div class="navbar-start">
-            <a href="#about" class="navbar-item">About</a>
-            <a href="#portfolio" class="navbar-item">Portfolio</a>
-            <a href="#contact" class="navbar-item">Contact</a>
-            <a href="#about" class="navbar-item">About</a>
-            <a href="#portfolio" class="navbar-item">Portfolio</a>
-            <a href="#contact" class="navbar-item">Contact</a>
+            <!-- <div class="navbar-item">... pulsing dot bridge ...</div> -->
         </div>
         <div class="navbar-end">
-            <!-- <div 
-                class="{`navbar-burger navbar-item navbar-link has-dropdown is-hoverable is-arrowless${active?' is-active':''}`}"
-                on:click={activate}
-                on:pointerenter={activate}
-                on:pointerleave={activate}
-            >
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
-                <div class="{`navbar-dropdown is-right${active?' is-active':''}`}">
-                    <div class="navbar-item">About</div>
-                    <div class="navbar-item">Portfolio</div>
-                    <div class="navbar-item">Contact</div>
-                    <hr class="navbar-divider"/>
-                    <div class="navbar-item">Resume</div>
-                    <div class="navbar-item">Github</div>
-                    <div class="navbar-item">Blog</div>
-                </div>
-            </div> -->
+            <a href="#about" class="navbar-item">About</a>
+            <a href="#portfolio" class="navbar-item">Portfolio</a>
+            <a href="#contact" class="navbar-item">Contact</a>
+            <a href="#about" class="navbar-item">Resume</a>
+            <a href="#portfolio" class="navbar-item">Github</a>
+            <!-- <a href="#contact" class="navbar-item">Blog (Coming Soon)</a> -->
         </div>
     </div>
 </nav>
-
+    
 <style>
-
+    .navbar-menu {justify-content:space-between; margin: 0 auto;}
 </style>
