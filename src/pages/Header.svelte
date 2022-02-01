c40970fbafe2e499ea80c69c4ea06a9e8cf22985
elikya <elikyabokanga@gmail.com>
2022-02-01 18:07:34 -0500

    header responsiveness complete

--- src/pages/Header.svelte
+++ src/pages/Header.svelte
@@ -5,9 +5,11 @@
     let index = []
     let title = "coder"
     
+    let hello
+    let word
+    let padding_top    
 
     function titleSwitch (t) {
-        // console.log(title)
         const current = titles.findIndex((w) => {return w === t})
         const next = current < titles.length-1 ? current+1 : 0
         index = [current, next]
@@ -15,29 +17,32 @@
         setTimeout(() => {titleSwitch(title)}, 3000)
     }
 
-    onMount(() => titleSwitch(title))
+    onMount(() => {
+        titleSwitch(title)
+        padding_top = `${(window.innerHeight-hello-word)/2}px`
+    })
 
     function roll(node, {delay=0, duration=1000, i}) {
         // console.log(node.textContent, title, i)
         if (title[i] === node.textContent) {
             return {delay, duration, css: t => `
                 transform: rotateX(${i%2===0? -90+t*90: 90-t*90}deg)
-                translateZ(2vw)
+                translateZ(7vw)
             `}
         } else {
             return {delay, duration, css: t => `
                 transform: rotateX(${i%2===1? -90+t*90: 90-t*90}deg)
-                translateZ(2vw)
+                translateZ(7vw)
             `}
         } 
     }
 </script>
 
 <header class="hero is-dark">
-    <div class="hero-body py-0">
-        <h1 class="title m-0">Hi,<br/>I'm Eli</h1>
+    <div class="hero-body pb-0" style:padding-top={padding_top}>
+        <h1 class="title m-0" bind:clientHeight={hello}>Hi,<br/>I'm Eli</h1>
         {#key title}
-            <div id="wordlock" class='title m-0 is-flex direction-row'>
+            <div id="wordlock" class='title m-0 is-flex direction-row' bind:clientHeight={word}>
                 {#each title as letter, i}
                     <div
                         in:roll="{{i:i}}"
