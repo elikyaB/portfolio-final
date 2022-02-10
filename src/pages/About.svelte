86c57f8a3718d518ec79127f922c3a614714da54
elikya <elikyabokanga@gmail.com>
2022-02-09 23:59:27 -0500

    idk

--- src/pages/About.svelte
+++ src/pages/About.svelte
@@ -1,6 +1,27 @@
 <script>
+    import { fade } from 'svelte/transition'
+    import { onMount } from 'svelte'
     let sY
     let vH
+    const bio = [
+        "For a long time I've wanted to code, from TI-89 math hacks and VBScript stack overflow 'viruses' in high school to being an Excel macro maximalist working full-time in college. In 2021 I finally decided to bite the bullet and pursue my dreams in app, crypto, and game development.",
+    ]
+
+    // function checkVar() {
+    //     console.log(sY, vH)
+    //     setTimeout(()=>{checkVar()}, 20)
+    // }
+
+    // onMount(() => checkVar())
+
+    function sticky(node, {duration = 400, i}) {
+        console.log(node.textContent, i)
+        const o = +getComputedStyle(node).opacity
+
+        return {duration, css: t => `
+            opacity: ${t * o}
+        `}
+    }
 </script>
 
 <svelte:window bind:scrollY={sY} bind:innerHeight={vH}/>
@@ -12,13 +33,22 @@
         </figure>
     </div>
     <div class="column">
-        <div class="block pl-3">
-            For a long time I've wanted to code, from TI-89 math hacks and VBScript stack overflow 'viruses' in high school to being an Excel macro maximalist working full-time in college. In 2021 I finally decided to bite the bullet and pursue my dreams in app, crypto, and game development.
-        </div>
+        {#each bio as note, i}
+            <!-- {#if sY>=(1+i/(bio.length*2))*vH-52} -->
+                <div class="block pl-3">
+                    {note}
+                </div>
+            <!-- {/if} -->
+        {/each}
     </div>
 </section>
 
 <style>
     #about {overflow: hidden;}
     img {position: relative; z-index: -1;}
+    .block {
+        transform: translate3d(10,10,10);
+        outline: 2px dashed black;
+        background: linear-gradient(165deg, yellow 0 50%, white 50% 100%);
+    }
 </style>
