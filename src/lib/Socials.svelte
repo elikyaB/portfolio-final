63f4f1366d66fc84d45e4c05b534166c45475820
elikya <elikyabokanga@gmail.com>
2022-03-21 15:52:10 -0400

    socials

---
+++ src/lib/Socials.svelte
@@ -0,0 +1,35 @@
+<script>
+
+</script>
+
+<aside class="socials">
+    <span class="icon is-large">
+        <a href="mailto:elikyabokanga@gmail.com">
+            <i class="fa-solid fa-envelope fa-xl"/>
+        </a>
+    </span>
+    <span class="icon is-large">
+        <a href="https://www.linkedin.com/in/ebokanga/">
+            <i class="fa-brands fa-linkedin-in fa-xl"/>
+        </a>
+    </span>
+    <span class="icon is-large">
+        <a href="https://twitter.com/eliB3webdev">
+            <i class="fa-brands fa-twitter fa-xl"/>
+        </a>
+    </span>
+    <span class="icon is-large">
+        <a href="https://github.com/elikyaB">
+            <i class="fa-brands fa-github fa-xl"/>
+        </a>
+    </span>
+    <span class="icon is-large">
+        <a href="https://codepen.io/Elikya">
+            <i class="fa-brands fa-codepen fa-xl"/>
+        </a>
+    </span>
+</aside>
+
+<style>
+    aside {display:flex; justify-content: space-around;}
+</style>
