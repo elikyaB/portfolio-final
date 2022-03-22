6802d983e732750c1acccae5c458d6b8ac2ce830
elikya <elikyabokanga@gmail.com>
2022-03-22 15:21:57 -0400

    minimalist footer done

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,1 +1,17 @@
-<footer>foot</footer>
+<script>
+    
+</script>
+
+<footer class="has-background-dark has-text-light pb-5">
+    <div class="container">
+        <div>
+            Designed & Built by<br/>
+            &copy; 2022 Elikya Bokanga
+        </div>
+    </div>
+</footer>
+
+<style>
+    /* footer {} */
+    div {text-align: center;}
+</style>
--- src/lib/Home/Contact.svelte
+++ src/lib/Home/Contact.svelte
@@ -1,10 +1,15 @@
 <script>
-    import Form from "../Form.svelte";
+    import Socials from "../Socials.svelte";
+    // testimonials?
 </script>
 
 <section id="contact" class="page--with-nav has-background-dark has-text-light">
     <div class="container">
-        
+        <h1 class="heading block has-text-warning">Let's get in touch</h1>
+        <p class="block">I'm open to opportunities of all kinds in the Boston area or remote. Whether it's a small passion project of yours or a corporate product, I'm always available to consult or collaborate.</p>
+        <button class="button block is-warning is-outlined">Say hello</button>
+        <p class="block">If you want to keep a pulse on where I'm headed, I'm active on these platforms:</p>
+        <Socials/>
     </div>
 </section>
 
